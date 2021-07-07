ca968bd1ffb09e408794ed0e97d8bf97e9655ce7
Szomor János <jszomor@gmail.com>
2021-07-07 12:04:45 +0200

    Environment variables added

---
+++ Server/OpticLtd.Api/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpticLtd.Api.Controllers
+{
+  public class OrderController
+  {
+  }
+}
---
+++ Server/OpticLtd.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpticLtd.Api.Mapping
+{
+  public class MappingProfile
+  {
+  }
+}
