6763813b6ef4161248cba2f41b505c54dd25ec26
Szomor János <jszomor@gmail.com>
2021-07-16 15:04:51 +0200

    pipeline progress

--- Server/OpticLtd.Api/Startup.cs
+++ Server/OpticLtd.Api/Startup.cs
@@ -1,16 +1,14 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using OpticLtd.BusinessLogic.Mediator;
+using OpticLtd.BusinessLogic.Product.Queries;
+using OpticLtd.Data;
 
 namespace OpticLtd.Api
 {
@@ -26,8 +24,11 @@ namespace OpticLtd.Api
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
+      services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Defaultconnection")));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
+      services.AddAutoMapper(typeof(Startup));
       services.AddControllers();
+      services.AddMediatR(typeof(GetProducts));
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpticLtd.Api", Version = "v1" });
---
+++ Server/OpticLtd.BusinessLogic/Mediator/TransactionBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Storage;
+using OpticLtd.Data;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpticLtd.BusinessLogic.Mediator
+{
+  public class TransactionBehavior<TRequest, TResult> : IPipelineBehavior<TRequest, TResult>
+  {
+    private readonly AppDbContext _context;
+
+    public TransactionBehavior(AppDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<TResult> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResult> next)
+    {
+      using var transaction = await _context.Database.BeginTransactionAsync();
+
+      try
+      {
+        var result = await next();
+        await transaction.CommitAsync();
+        return result;
+      }
+      catch (Exception)
+      {
+        await transaction.RollbackAsync();
+        throw;
+      }
+    }
+  }
+}
