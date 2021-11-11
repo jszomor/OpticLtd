12791c9d27abb6bedbd22fa0d1cbb2aa11e6226c
Szomor János <jszomor@gmail.com>
2021-11-11 15:19:12 +0100

    ProductControllerTest progress

--- OpticLtd.APITest/ControllerTest.cs
+++ OpticLtd.APITest/ControllerTest.cs
@@ -1,7 +1,18 @@
+using Autofac.Extras.Moq;
+using AutoMapper;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using OpticLtd.Api.Controllers;
 using OpticLtd.BusinessLogic.Product.Queries;
+using OpticLtd.Data;
+using OpticLtd.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static OpticLtd.BusinessLogic.Product.Queries.GetProduct;
 
 namespace OpticLtd.APITest
 {
@@ -16,11 +27,62 @@ namespace OpticLtd.APITest
     [Test]
     public void ProductControllerTest()
     {
-      //var mockProduct = new Mock<GetProduct.Query>();
-      //mockProduct.Setup(p => p.List())
-      //  .ReturnsAsync()
+      //using (var mock = AutoMock.GetLoose())
+      //{
 
-      Assert.Pass();
+      //var mockQuery = new Mock<Query>();
+
+      var query = new Query();
+
+      var mockHandler = new Mock<IRequestHandler<Query, List<Product>>>()
+        .Setup(x => x.Handle(query, CancellationToken.None))
+        .Returns(GetSampleProduct());
+
+      var mockMediator = new Mock<IMediator>();
+      var mockLogger = new Mock<ILogger<ProductController>>();
+
+      //mock.Mock<Handler>()
+      //    .Setup(x => x.Handle(query, CancellationToken.None))
+      //    .Returns(GetSampleProduct());
+
+      //var cls = mock.Create<Handler>();
+      //var expected = GetSampleProduct();
+
+      //var actual = cls.Handle(query, CancellationToken.None);
+
+      //var productController = new ProductController(_mapper, _mediator, _logger);
+      //var result = await productController.GetProduct(mockQuery);
+
+      //Assert.True(actual != null);
+      //Assert.Equal(expected.Count, actual.Count);
+
+      //for (int i = 0; i < expected.Count; i++)
+      //{
+      //  Assert.Equal(expected[i].FirstName, actual[i].FirstName);
+      //  Assert.Equal(expected[i].LastName, actual[i].LastName);
+      //}
+      //}
+    }
+
+    private Task<List<Product>> GetSampleProduct()
+    {
+      List<Product> products = new List<Product>
+      {
+        new Product
+        {
+          ProductId = 1,
+          ProductCategory = "Lencse",
+          ProductName = "Hoya"
+        },
+        new Product
+        {
+          ProductId = 2,
+          ProductCategory = "Szemüveg",
+          ProductName = "Rayban"
+        }
+
+      };
+      return Task.FromResult(products);
     }
   }
 }
