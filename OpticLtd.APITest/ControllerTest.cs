16fbc01016ab8810bd6444f9495b817231c15c18
Szomor János <jszomor@gmail.com>
2021-11-11 09:37:59 +0100

    Api test project init

---
+++ OpticLtd.APITest/ControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace OpticLtd.APITest
+{
+  public class ControllerTest
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
